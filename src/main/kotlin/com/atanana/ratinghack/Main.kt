fb018d73084b775e4acb2fc9195dbe2bb87c8018
atanana <tanana@email.ru>
2018-02-04 13:40:12 +0300

    main page init

--- src/main/kotlin/com/atanana/ratinghack/Main.kt
+++ src/main/kotlin/com/atanana/ratinghack/Main.kt
@@ -3,6 +3,9 @@ package com.atanana.ratinghack
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.application.install
+import io.ktor.content.defaultResource
+import io.ktor.content.resources
+import io.ktor.content.static
 import io.ktor.features.CallLogging
 import io.ktor.features.DefaultHeaders
 import io.ktor.http.ContentType
@@ -14,14 +17,15 @@ fun Application.module() {
     install(DefaultHeaders)
     install(CallLogging)
     install(Routing) {
-        get("/") {
-            call.respondText("My Example Blog2", ContentType.Text.Html)
-        }
-
         get("/tournament/{n}") {
             val tournamentId = call.parameters["n"]!!.toInt()
             val info = TeamsOnTournamentInfoProvider.getInfo(tournamentId)
             call.respondText(info, ContentType.Application.Json)
+        }
+
+        static {
+            defaultResource("index.html", "web")
+            resources("web")
         }
     }
 }
