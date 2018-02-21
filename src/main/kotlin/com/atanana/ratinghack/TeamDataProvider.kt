6967389e914356ec02946cbc99506ab845d2fefc
atanana <tanana@email.ru>
2018-02-21 09:13:35 +0300

    teams data cache

--- src/main/kotlin/com/atanana/ratinghack/TeamDataProvider.kt
+++ src/main/kotlin/com/atanana/ratinghack/TeamDataProvider.kt
@@ -1,28 +1,35 @@
 package com.atanana.ratinghack
 
 import com.beust.klaxon.Klaxon
+import com.google.common.cache.CacheBuilder
+import com.google.common.cache.CacheLoader
+import java.util.concurrent.TimeUnit
 
 object TeamDataProvider {
-    private val klaxon = Klaxon()
+    private val teamInfoCache = CacheBuilder.newBuilder()
+            .maximumSize(10000)
+            .build(CacheLoader.from<Int, TeamInfo?> { teamId ->
+                val data = Connector.teamInfo(teamId!!)
+                val rawTeamInfo = Klaxon().parseArray<RawTeamInfo>(data)?.firstOrNull()
+                rawTeamInfo?.toInfo()
+            })
 
-    fun getTeamData(teamId: Int, tournametId: Int): TeamData? {
-        val teamInfo = getTeamInfo(teamId)
-        val teamResults = getTeamResults(teamId, tournametId)
+    private val teamResultsCache = CacheBuilder.newBuilder()
+            .expireAfterAccess(5, TimeUnit.DAYS)
+            .build(CacheLoader.from<Pair<Int, Int>, TeamResult?> {
+                val (teamId, tournamentId) = it!!
+                val data = Connector.teamsResultsData(teamId)
+                TeamResultsExtractor.getTeamResult(data, tournamentId)
+            })
+
+
+    fun getTeamData(teamId: Int, tournamentId: Int): TeamData? {
+        val teamInfo = teamInfoCache.get(teamId)
+        val teamResults = teamResultsCache.get(Pair(teamId, tournamentId))
         return if (teamInfo != null && teamResults != null) {
             TeamData(teamInfo, teamResults)
         } else {
             null
         }
-    }
-
-    private fun getTeamInfo(teamId: Int): TeamInfo? {
-        val data = Connector.teamInfo(teamId)
-        val rawTeamInfo = klaxon.parseArray<RawTeamInfo>(data)?.firstOrNull()
-        return rawTeamInfo?.toInfo()
-    }
-
-    private fun getTeamResults(teamId: Int, tournametId: Int): TeamResult? {
-        val data = Connector.teamsResultsData(teamId)
-        return TeamResultsExtractor.getTeamResult(data, tournametId)
     }
 }
