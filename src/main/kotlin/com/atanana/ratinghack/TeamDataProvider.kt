e6b8e4c7e174a44ff224206510096d4f1624a5c1
atanana <tanana@email.ru>
2018-02-25 13:25:20 +0300

    disable team results cache

--- src/main/kotlin/com/atanana/ratinghack/TeamDataProvider.kt
+++ src/main/kotlin/com/atanana/ratinghack/TeamDataProvider.kt
@@ -5,7 +5,6 @@ import com.beust.klaxon.JsonObject
 import com.beust.klaxon.Parser
 import com.google.common.cache.CacheBuilder
 import com.google.common.cache.CacheLoader
-import java.util.concurrent.TimeUnit
 
 object TeamDataProvider {
     private val parser = Parser()
@@ -17,22 +16,18 @@ object TeamDataProvider {
                 ((parser.parse(StringBuilder(data)) as JsonArray<*>).firstOrNull() as JsonObject).toTeamInfo()
             })
 
-    private val teamResultsCache = CacheBuilder.newBuilder()
-            .expireAfterAccess(5, TimeUnit.DAYS)
-            .build(CacheLoader.from<Pair<Int, Int>, TeamResult?> {
-                val (teamId, tournamentId) = it!!
-                val data = Connector.teamsResultsData(teamId)
-                TeamResultsExtractor.getTeamResult(data, tournamentId)
-            })
-
-
     fun getTeamData(teamId: Int, tournamentId: Int): TeamData? {
         val teamInfo = teamInfoCache.get(teamId)
-        val teamResults = teamResultsCache.get(Pair(teamId, tournamentId))
+        val teamResults = getTeamResult(teamId, tournamentId)
         return if (teamInfo != null && teamResults != null) {
             TeamData(teamInfo, teamResults)
         } else {
             null
         }
+    }
+
+    private fun getTeamResult(teamId: Int, tournamentId: Int): TeamResult? {
+        val data = Connector.teamsResultsData(teamId)
+        return TeamResultsExtractor.getTeamResult(data, tournamentId)
     }
 }
