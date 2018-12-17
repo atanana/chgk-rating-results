22dd2abb4db647eec7fcd57a8b6cb101e182b813
Andrei <tanana@email.ru>
2018-12-17 23:54:30 +0300

    update deploy

--- src/main/kotlin/com/atanana/ratinghack/Main.kt
+++ src/main/kotlin/com/atanana/ratinghack/Main.kt
@@ -1,5 +1,6 @@
 package com.atanana.ratinghack
 
+import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.features.CallLogging
@@ -10,32 +11,27 @@ import io.ktor.http.content.static
 import io.ktor.response.respondText
 import io.ktor.routing.get
 import io.ktor.routing.routing
-import io.ktor.server.engine.embeddedServer
-import io.ktor.server.netty.Netty
 
-fun main(args: Array<String>) {
-    val server = embeddedServer(Netty, port = 8080) {
-        install(DefaultHeaders)
-        install(CallLogging)
+fun Application.main() {
+    install(DefaultHeaders)
+    install(CallLogging)
 
-        routing {
-            get("/tournament/{n}") {
-                val tournamentId = call.parameters["n"]!!.toInt()
-                val info = TeamsOnTournamentInfoProvider.getInfo(tournamentId)
-                call.respondText(info, ContentType.Application.Json)
-            }
+    routing {
+        get("/tournament/{n}") {
+            val tournamentId = call.parameters["n"]!!.toInt()
+            val info = TeamsOnTournamentInfoProvider.getInfo(tournamentId)
+            call.respondText(info, ContentType.Application.Json)
+        }
 
-            get("/") {
-                val json = InterestingTournamentsProvider.getInterestingTournamentsJson()
-                val page = javaClass.getResource("/web/index.html").readText()
-                val resultPage = page.replaceFirst("%tournaments_data%", json.toJsonString())
-                call.respondText(resultPage, ContentType.Text.Html)
-            }
+        get("/") {
+            val json = InterestingTournamentsProvider.getInterestingTournamentsJson()
+            val page = javaClass.getResource("/web/index.html").readText()
+            val resultPage = page.replaceFirst("%tournaments_data%", json.toJsonString())
+            call.respondText(resultPage, ContentType.Text.Html)
+        }
 
-            static {
-                resources("web")
-            }
+        static {
+            resources("web")
         }
     }
-    server.start(wait = true)
 }
