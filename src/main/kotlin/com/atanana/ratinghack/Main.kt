e06e7ec86abee49099c709d5a71d46ae67508f0e
Andrei <tanana@email.ru>
2018-12-17 23:43:01 +0300

    update main

--- src/main/kotlin/com/atanana/ratinghack/Main.kt
+++ src/main/kotlin/com/atanana/ratinghack/Main.kt
@@ -1,6 +1,5 @@
 package com.atanana.ratinghack
 
-import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.features.CallLogging
@@ -9,28 +8,34 @@ import io.ktor.http.ContentType
 import io.ktor.http.content.resources
 import io.ktor.http.content.static
 import io.ktor.response.respondText
-import io.ktor.routing.Routing
 import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
 
-fun Application.module() {
-    install(DefaultHeaders)
-    install(CallLogging)
-    install(Routing) {
-        get("/tournament/{n}") {
-            val tournamentId = call.parameters["n"]!!.toInt()
-            val info = TeamsOnTournamentInfoProvider.getInfo(tournamentId)
-            call.respondText(info, ContentType.Application.Json)
-        }
+fun main(args: Array<String>) {
+    val server = embeddedServer(Netty, port = 8080) {
+        install(DefaultHeaders)
+        install(CallLogging)
 
-        get("/") {
-            val json = InterestingTournamentsProvider.getInterestingTournamentsJson()
-            val page = javaClass.getResource("/web/index.html").readText()
-            val resultPage = page.replaceFirst("%tournaments_data%", json.toJsonString())
-            call.respondText(resultPage, ContentType.Text.Html)
-        }
+        routing {
+            get("/tournament/{n}") {
+                val tournamentId = call.parameters["n"]!!.toInt()
+                val info = TeamsOnTournamentInfoProvider.getInfo(tournamentId)
+                call.respondText(info, ContentType.Application.Json)
+            }
 
-        static {
-            resources("web")
+            get("/") {
+                val json = InterestingTournamentsProvider.getInterestingTournamentsJson()
+                val page = javaClass.getResource("/web/index.html").readText()
+                val resultPage = page.replaceFirst("%tournaments_data%", json.toJsonString())
+                call.respondText(resultPage, ContentType.Text.Html)
+            }
+
+            static {
+                resources("web")
+            }
         }
     }
+    server.start(wait = true)
 }
